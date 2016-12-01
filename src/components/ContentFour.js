151e72195da40674d9fa0bbd3e05d39c2f6fbe4c
orangeC <18332563755@163.com>
2016-12-01 21:35:44 +0800

    style

--- src/components/ContentFour.js
+++ src/components/ContentFour.js
@@ -13,11 +13,10 @@ class ContentFour extends Component{
 				width:"100%",
 				padding:"30px 10px",
 				float:"left",
-				textAlign:"center"
 			},
 			main:{
 				width:"100%",
-				maxWidth:"1024px",
+				maxWidth:"1090px",
 				margin:"0 auto",
 				padding:"20px",
 				display:"flex",
@@ -33,34 +32,29 @@ class ContentFour extends Component{
 			span:{
 				fontWeight:"600"
 			},
-			p:{
-				textAlign:"center",
-				color:"rgb(123, 123, 123)",
-				textTransform: "uppercase",
-				fontSize:"0.9em",
-				marginBottom:"10px"
+			h4:{
+				textAlign: "center",
+    			lineHeight: "2",
+    			color: "#fff",
+    			margin:"0",
+    			fontSize:"1.1em",
+    			fontWeight:"600"
 			},
-			short:{
-				width:"4em",
-				height:"3px",
-				backgroundColor:"red",
-				margin:"10px auto 25px"
+			p:{
+				width: "100%",
+    			padding: "20px 10px",
+    			overflow: "hidden"
 			},
 			h3:{
 				marginTop: "20px",
     			marginBottom: "10px",
     			fontWeight:"300",
-    			fontSize:"1.5em"
-			},
-			pp:{
-				color:"#282828",
-				lineHeight:"1.9em",
-				fontSize:"0.9em",
-				margin:"0 0 15px"
+    			fontSize:"1.5em",
+    			textAlign:"center"
 			},
 			img:{
-				width:"100%",
-				marginTop:"10px"
+				marginTop:"5px",
+				border:"0"
 			},
 			card:{
 				width:"32%",
@@ -70,34 +64,34 @@ class ContentFour extends Component{
 		return (
 				<div style={styles.root} className="ContentFour">
 					<h3 style={styles.h3} >茫茫人海中   <span style={styles.span} >你是哪一个</span></h3>
-					<div style={styles.main} >
-						<div style={styles.card} >
-							<h4>在校大学生</h4>
+					<div style={styles.main} className="four-main" >
+						<div style={styles.card} className="four-card" >
+							<h4 style={styles.h4} className="one" >在校大学生</h4>
 							<img src={Bg3} />
-							<img src={Bg} />
-							<div >
+							<img src={Bg} style={styles.img} />
+							<div style={styles.p} >
 								<p>你还在为大学里学的课程和企业要求不匹配而烦恼吗？</p>
 								<p>你还在担心大学毕业后找不到理想工作而忧虑吗？</p>
 								<p>你还在为花了父母几万块钱，脑袋里面空空而痛苦吗？</p>
 								<p>你还在不能给父母最后的回报而伤心吗？</p>
 							</div>
 						</div>
-						<div style={styles.card} >
-							<h4>在职</h4>
+						<div style={styles.card} className="four-card" >
+							<h4 style={styles.h4} className="two" >在职</h4>
 							<img src={Bg4} />
-							<img src={Bg1} />
-							<div >
+							<img src={Bg1} style={styles.img} />
+							<div style={styles.p} >
 								<p>你还在企业边缘岗，每天看主管脸色吗？</p>
 								<p>你还在因为自己没有专业技能，无法提升而烦恼吗？</p>
 								<p>你还在单位浑浑噩噩的混日子吗？</p>
 								<p>你还在为迟迟不加薪而着急吗？</p>
 							</div>
 						</div>
-						<div style={styles.card} >
-							<h4>没有工作</h4>
+						<div style={styles.card} className="four-card" >
+							<h4 style={styles.h4} className="three" >没有工作</h4>
 							<img src={Bg5} />
-							<img src={Bg2} />
-							<div >
+							<img src={Bg2} style={styles.img} />
+							<div style={styles.p} >
 								<p>你还在为毕业了没有找到满意的工作而痛苦吗？</p>
 								<p>你还在为没有一技之长而着急吗？</p>
 								<p>你还在为不能自食其力而苦恼吗？</p>
