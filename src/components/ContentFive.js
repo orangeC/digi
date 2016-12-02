dc4b85225b5fa3eb62aaf49bb15184743015ef79
orangeC <18332563755@163.com>
2016-12-02 11:00:26 +0800

    contentFive top completed

--- src/components/ContentFive.js
+++ src/components/ContentFive.js
@@ -16,47 +16,36 @@ class ContentFive extends Component{
     			margin: "0px auto",
     			borderBottom: "1px solid rgb(204, 204, 204)"
 			},
-			h1:{
-				textAlign:"center",
-				marginBottom:"20px",
-				color:"rgb(231, 33, 14)",
-				fontSize:"36px"
-			},
 			span:{
 				fontWeight:"400"
 			},
 			p:{
 				fontWeight: "600",
-    			fontSize: "1em",
-    			paddingLeft: "5px"
-			},
-			short:{
-				width:"4em",
-				height:"3px",
-				backgroundColor:"red",
-				margin:"0 auto"
-			},
-			pp:{
-				margin:"15px auto",
-				fontSize:"1.2em"
+    			fontSize: "1.1em",
+    			paddingLeft: "5px",
+    			paddingBottom:"10px"
 			},
 			img:{
-				display:"block",
-				maxWidth:"960px",
-				width:"90%",
-				margin:"0 auto 20px"
+				width:"100%",
+				maxWidth:"200px",
+				marginTop: '20px',
+    			marginBottom: '20px'
+			},
+			a:{
+				textAlign:"center",
+				padding:"50px 0"
 			}
 		}
 		return (
 				<div style={styles.root} className="ContentFive">
 					<div style={styles.top} >
-						<img  src={Bg} />
+						<img  src={Bg} style={styles.img} />
 						<p style={styles.p} >pc端模块：<span style={styles.span} >div＋css网站重构，浏览器兼容，网站交互特效，数据交互技术，面向对象编程思想及模块化开发原理</span></p> 
 						<p style={styles.p} >移动端模块：<span style={styles.span} >css3动画，响应式设计，H5开发，微信场景开发技术，Web App开发</span></p> 
 						<p style={styles.p} >全栈模块：<span style={styles.span} >最新mean技术栈，es6，nodejs，reactjs组件化开发等技术</span></p> 
 						<p style={styles.p} >全国首创ui+web前端课程，熟悉完整工作流程，减少沟通阻力，实现与ui设计师工作完美对接</p> 
-						<div>
-							<a href="#" className="search-more" >了解详细课程内容</a>
+						<div style={styles.a}>
+							<a href="#" className="search-more"  >了解详细课程内容</a>
 						</div>
 					</div>
 					<div style={styles.center} ></div>
